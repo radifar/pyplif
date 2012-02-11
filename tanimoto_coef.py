60b54c01c935a19fc6eb9504dcf25ed53c4faefb
m.radifar05@gmail.com <m.radifar05@gmail.com@8ac73149-be81-7d20-fe2b-3dc783c49c7d>
2012-02-11 12:49:16 +0000

    improve counttc & fix serious bug in gettcfromdict function

--- tanimoto_coef.py
+++ tanimoto_coef.py
@@ -4,19 +4,14 @@ from bitarray import bitarray
 
 def counttc(refbit, tgtbit):
     """Measure Tanimoto coefficient of reference bit (refbit) and target bit (tgtbit)"""
-    refcopy = refbit.copy()
-    tgtcopy = tgtbit.copy()
-    a = b = c = 0
-    
+    a = refbit.count()
+    b = tgtbit.count()
+    c = 0
     while refbit.length():
-        if refbit.pop():
-            a += 1
-        if tgtbit.pop():
-            b += 1
-        if refcopy.pop() & tgtcopy.pop():
+        if refbit.pop() & tgtbit.pop():
             c += 1
-        if refcopy.length() < tgtcopy.length():
-            tgtcopy.pop()
+        if refbit.length() < tgtbit.length():
+            tgtbit.pop()
 
     tc = float(c)/(a+b-c)
     
@@ -26,8 +21,9 @@ def gettcfromdict(dict1, dict2, residuechoice):
     bit1 = bitarray()
     bit2 = bitarray()
     for residue in residuechoice:
-        bit1.append(dict1[residue])
-        bit2.append(dict2[residue])
+        bit1.extend(dict1[residue])
+        bit2.extend(dict2[residue])
+
     tcfromdict = counttc(bit1, bit2)
     
     return tcfromdict
