a61cb31a8522e0044552661f46a441f121eb629f
m.radifar05@gmail.com <m.radifar05@gmail.com@8ac73149-be81-7d20-fe2b-3dc783c49c7d>
2012-02-15 06:54:44 +0000

    Fixing the setup shell script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,10 @@ if  `cat $HOME/.bashrc | grep 'pyplif'`; then
     echo "PyPLIF is already installed"
     exit
 else
+    if [ -e $HOME/.pyplif ]; then
+        rm -r $HOME/.pyplif
+    fi
     cp -r pyplif $HOME/.pyplif
-    echo 'export pyplif="$HOME/.pyplif/pyplif.py"' >> $HOME/.bashrc
+    echo "alias pyplif='$HOME/.pyplif/pyplif.py'" >> $HOME/.bashrc
     echo "PyPLIF successfully installed"
 fi
