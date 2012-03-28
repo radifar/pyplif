875a1a8e210a59fc5946743289912b550cb73ec3
m.radifar05@gmail.com <m.radifar05@gmail.com@8ac73149-be81-7d20-fe2b-3dc783c49c7d>
2012-03-28 03:17:02 +0000

    adding 'uninstall' and 'force' option to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,42 @@
 #!/bin/bash
 
-if  `cat $HOME/.bashrc | grep 'pyplif'`; then
-    echo "PyPLIF is already installed"
-    exit
-else
-    if [ -e $HOME/.pyplif ]; then
-        rm -r $HOME/.pyplif
+# A script to copy the pyplif to hidden folder inside
+# HOME folder then add a 'command-line shortcut'
+# to PyPLIF inside that hidden folder.
+if [ $1 > 0 ]  ; then
+    if  [ $1 = 'uninstall' ]; then
+        if [ -e $HOME/.pyplif ]; then
+            rm -r $HOME/.pyplif
+            gawk '$0 !~/alias pyplif/ { print $0 }' $HOME/.bashrc > .bashrc.tmp
+            cat .bashrc.tmp > $HOME/.bashrc
+            rm .bashrc.tmp
+            echo "PyPLIF has been successfully uninstalled"
+        else
+            echo "PyPLIF never been installed before"
+        fi
+    elif [ $1 = 'force' ]; then
+        if [ -e $HOME/.pyplif ]; then
+            rm -r $HOME/.pyplif
+        else
+            echo "alias pyplif='$HOME/.pyplif/pyplif.py'" >> $HOME/.bashrc
+        fi
+        cp -r pyplif $HOME/.pyplif
+        chmod +x $HOME/.pyplif/pyplif.py
+        echo "PyPLIF successfully installed"
+    else
+        echo "Wrong Argument"
     fi
-    cp -r pyplif $HOME/.pyplif
-    chmod +x $HOME/.pyplif/pyplif.py
-    echo "alias pyplif='$HOME/.pyplif/pyplif.py'" >> $HOME/.bashrc
-    echo "PyPLIF successfully installed"
+else  
+    if  `cat $HOME/.bashrc | grep 'pyplif'`; then
+        echo "PyPLIF is already installed"
+        exit
+    else
+        if [ -e $HOME/.pyplif ]; then
+            rm -r $HOME/.pyplif
+        fi
+        cp -r pyplif $HOME/.pyplif
+        chmod +x $HOME/.pyplif/pyplif.py
+        echo "alias pyplif='$HOME/.pyplif/pyplif.py'" >> $HOME/.bashrc
+        echo "PyPLIF successfully installed"
+    fi
 fi
